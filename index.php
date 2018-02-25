d3858a7ac67191a2ffa86c7a8076539a9d96a803
Silvamorta <sargatanas94@mail.ru>
2018-02-25 23:54:29 +0500

    Добавлена версия css

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <title>StyleGuide</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?version=2">
     <link rel="stylesheet" href="css/normalize.css">
 </head>
 <body class="body">
